55b1f6b5eaad3132af4fd29e7e174d4fc691bded
ortal <ortallik@gmail.com>
2021-05-02 09:30:36 +0300

    Add project files.

---
+++ ColmanAppStore/Models/Apps.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ColmanAppStore.Models
+{
+    public class Apps
+    {
+        //key
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public float Price { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime publishDate { get; set; }
+
+        public Category Category { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public float Size { get; set; }
+
+        public string Logo { get; set; }
+
+        public string Images { get; set; }
+
+        public float Raiting { get; set; }
+
+        public string Review { get; set; }
+
+
+
+        public string DeveloperName { get; set; }
+    }
+}
