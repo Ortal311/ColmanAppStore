1d37ed1c25acbe944f8173a6623cb049bb0ffb8c
Yuval Mirski <mirskiyuval7@gmail.com>
2021-08-03 18:29:18 +0300

    style changes

--- ColmanAppStore/Models/AppImage.cs
+++ ColmanAppStore/Models/AppImage.cs
@@ -18,6 +18,7 @@ namespace ColmanAppStore.Models
         [Required]
         public string Image { get; set; }
 
+        [Display(Name = "App name")]
         public int AppId { get; set; }
 
         [Display(Name = "App name")]
